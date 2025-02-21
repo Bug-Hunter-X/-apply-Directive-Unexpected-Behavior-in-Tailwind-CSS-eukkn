e24bb0a48eb42fa1d4737db73829646292efe5c9
BugHunter-X <askzenai@gmail.com>
2025-02-21 03:32:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,13 @@
+```javascript
+// Correct usage of the @apply directive within a class definition
+
+.my-class {
+  @apply bg-gray-200 p-4;
+}
+
+<div class="my-class">
+  <p>This is some text.</p>
+</div>
+
+//This applies the styles correctly.
+```
